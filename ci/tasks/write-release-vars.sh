889088582fb46d64d2122cbdd519ba23c464212a
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-10-19 15:37:39 +0100

    add grafana version to release notes

--- ci/tasks/write-release-vars.sh
+++ ci/tasks/write-release-vars.sh
@@ -8,6 +8,7 @@ release_name=$(bosh int grafana-boshrelease/config/final.yml --path /name)
 release_url="https://github.com/EngineerBetter/grafana-boshrelease/releases/download/${version}/grafana-final-release-${version}.tgz"
 release_tgz="final-release/grafana-final-release-${version}.tgz"
 release_sha1=$(sha1sum "${release_tgz}" | head -n1 | awk '{print $1}')
+grafana_tag=$(cat grafana-binary-version/tag)
 
 echo "${name}" > release-vars/name
 
@@ -19,6 +20,8 @@ echo "$commit" > release-vars/commit
 
 cat << EOF > release-vars/body
 Auto-generated release
+
+Grafana version: $grafana_tag
 
 ### Deployment
 \`\`\`yaml
