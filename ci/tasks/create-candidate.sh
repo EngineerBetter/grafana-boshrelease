eb2f5841bb87380dfba46dc74a3f463dd17eca4f
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-10-19 14:50:42 +0100

    Use full repo for creating release
    Using repo with ignore_paths was causing weird async issues with release versions

--- ci/tasks/create-candidate.sh
+++ ci/tasks/create-candidate.sh
@@ -4,6 +4,6 @@ set -e
 
 export ROOT_PATH=$PWD
 
-cd grafana-boshrelease
+cd grafana-boshrelease-push
 
 bosh create-release --tarball="${ROOT_PATH}/release/grafana-dev-release.tgz" --timestamp-version --force
